cebf3159737904cc2f6c962b806158483264440a
Micaelen Miranda <mirandamicaelen@gmail.com>
2019-07-06 22:10:20 -0300

    Criada a estrutura html assim como o array que contem os cards

---
+++ js/script.js
@@ -0,0 +1,62 @@
+const cardsArray = [
+
+    {
+        name: 'bulbasaur',
+        img: 'img/bulbasaur.gif',
+    },
+
+    {
+        name: 'psyduck',
+        img: 'img/psyduck.gif',
+    },
+
+    {
+        name: 'pikachu',
+        img: 'img/ditto.gif',
+    },
+
+    {
+        name: 'vaporeon',
+        img: 'img/vaporeon.gif',
+    },
+
+    {
+        name: 'espeon',
+        img: 'img/espeon.gif',
+    },
+
+    {
+        name: 'jigglypuff',
+        img: 'img/jigglypuff',
+    },
+
+    {
+        name: 'jolteon',
+        img: 'img/jolteon.gif',
+    },
+
+    {
+        name: 'snorlax',
+        img: 'img/snorlax.gif',
+    },
+
+    {
+        name: 'meowth',
+        img: 'img/meowth.gif',
+    },
+
+    {   
+        name: 'vulpix',
+        img: 'img/vulpix.gif',
+    },
+
+    {
+        name: 'squirtle',
+        img: 'img/squirtle.gif',
+    },
+
+    {
+        name: 'charmander',
+        img: 'charmander.gif'
+    },
+]
